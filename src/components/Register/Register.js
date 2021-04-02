900776bc6c678f509dda7a133ed40a2f35fcd377
balanabhijith4 <59792071+balanabhijith4@users.noreply.github.com>
2021-04-02 08:35:21 +0530

    Update Register.js

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://smartbrainlvapi.herokuapp.com/register', {
+    fetch('https://smartbrainlvapp.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
