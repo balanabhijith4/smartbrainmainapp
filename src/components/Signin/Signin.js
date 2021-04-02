a81eb05e5216ffa775c1917df9501015648c8942
balanabhijith4 <59792071+balanabhijith4@users.noreply.github.com>
2021-04-02 08:35:44 +0530

    Update Signin.js

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://smartbrainlvapi.herokuapp.com/signin', {
+    fetch('https://smartbrainlvapp.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
